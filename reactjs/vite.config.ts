2790bc943e9917b5eabf7433621f04736c873300
Steven Brown <smbrown1@student.fullsail.edu>
2023-10-06 18:19:28 -0400

    Refactor for updated proxy.

--- reactjs/vite.config.ts
+++ reactjs/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
 // https://vitejs.dev/config/
-export default defineConfig({
+export default function defineConfig() {
+  return {
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api_v1': {
+        target: process.env.VITE_PROXY_URL,
+        changeOrigin: true,
+      }
+    }
+  }
+} 
+}
+
